0cf4bf779d5dd410004b786c88122097e9dc8a04
simplykevine <umutonikevine2004@gmail.com>
2023-10-13 07:22:43 -0700

    up to date

--- 0x04-python-more_data_structures/103-tests.py
+++ 0x04-python-more_data_structures/103-tests.py
@@ -1,13 +1,15 @@
 import ctypes
 
-
-lib = ctypes.CDLL ('./libPython.so')
+lib = ctypes.CDLL('./libPython.so')
 
 lib.print_python_list.argtypes = [ctypes.py_object]
 lib.print_python_bytes.argtypes = [ctypes.py_object]
+
 s = b"Hello"
+
 lib.print_python_bytes(s);
 b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
+
 lib.print_python_bytes(b);
 b = b'What does the \'b\' character do in front of a string literal?';
 lib.print_python_bytes(b);
@@ -31,3 +33,4 @@ lib.print_python_list(l)
 l = ["Holberton"]
 lib.print_python_list(l)
 lib.print_python_bytes(l);
+
