9329ac8335a64a3401c94d32e9ff897477e0d08b
simplykevine <umutonikevine2004@gmail.com>
2023-10-30 15:15:50 -0700

    Kevine

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,112 +1,80 @@
 #!/usr/bin/python3
-""" calculates and return the area of the rectangle
-"""
-
+"""Defines a Rectangle class."""
 '''
-File_name: 3-rectangle.py
-Created: 30-OCT-2023
+File_name: 0-rectangle.py
+Created: 5-OCT-2023
 Author: UMUTONI Kevine (simplykevine)
-Size: large
+Size: Large
 Project: 0x08-python-more_classes
-Status: not yet submitted.
+Status: Not yet submitted.
 '''
-
 
 class Rectangle:
-    """
-    # Write a class Rectangle that defines a rectangle by:
-    # ....(based on 3-rectangle.py)
-    # Instantiation with optional width and height:
-    # ....def __init__(self, width=0, height=0):
-    # VARIABLE(" "):
-    # Rectangle_Area/Perimeter(int): String representation
-    # Return: Always 0, (Success).
-    """
+    """Represent a rectangle."""
 
-    """ 'class rectangle' defines the class is like a blueprint or template...
-    for creating objects...
-    while the 'pass' is used as a placeholder when you don't want to add...
-    any code in a block and this is just a sample of a 2D Polygon with a ...
-    ...4-Perpendicular Sides."""
     def __init__(self, width=0, height=0):
-        """
-        initializes rectangle
+        """Initialize a new Rectangle.
 
         Args:
-            width: width of rectangle
-            height: height of rectangle
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ '@property' decorator allows us to retrieve the value of...
-        'width' using 'rectangle_instance.width' instead of directly...
-        accessing the private attribute."""
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """The setter method is called when we assign a value to...
-        'rectangle_instance.width = value'.."""
-        if type(value) is not int:
-            raise TypeError('width must be an integer')
-        elif value < 0:
-            raise ValueError('width must be >= 0')
-        else:
-            self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
-        """ here, we define a greater method for the private attribute...
-        'height' using the '@property' decorator."""
+        """Get/set the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """inside this setter decorator, we perform the same validation...`
-        as for the 'width' setter, but applied to the 'height' attribute"""
-        if type(value) is not int:
-            raise TypeError('height must be an integer')
-        elif value < 0:
-            raise ValueError('height must be >= 0')
-        else:
-            self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
-        """ This method calculates and returns the area of the rectangle...
-        which is the product of its width and height. it uses the private
-        attributes'__width' and '__height' to perform the calculation"""
-        return self.__width * self.__height
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
 
     def perimeter(self):
-        """ Public instance method calculates and returns the perimeter...
-        of the rectangle.. if either the width or height is equal to 0,..
-        the perimeter is considered '0'. Otherwise, it calculates...
-        the perimeter using the formula: 2 * (width + height).
-        Again, it uses the private attributes '__width' and '__height'...
-        for the calculation..."""
+        """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """ '__str__()' method overrides the default string representation...
-        ...of the object when using 'str(rectangle_instance)'....
-        it creates a string representation of the triangle using the '#'..."""
+        """Return the printable representation of the Rectangle.
+
+        Represents the rectangle with the # character.
+        """
         if self.__width == 0 or self.__height == 0:
-            return ""
-        rectangle_str = ""
-        for _ in range(self.__height):
-            """if either the width or height is equal to '0' or an empty....
-            string is returned.. It uses a 'Loop' to build the string...
-            representation by concatenating the appropriate number of '#'...
-            characters for each row.."""
-            rectangle_str += "#" * self.__width + "\n"
-        return rectangle_str[:-1]
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
-        """This method returns a string representation of the object...
-        that can be used to recreate a new instance of the 'Rectangle class"""
-        return "Rectangle({}, {})".format(self.__width,
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
