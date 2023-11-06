0e2183a1da41fab9ca2966cc787fa86f9adc1b99
simplykevine <umutonikevine2004@gmail.com>
2023-11-06 13:04:29 -0800

    python_inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Public instance method: def area(self):"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+
