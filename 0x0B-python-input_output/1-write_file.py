360746d5127b3245e93f49dbbd52495865141ed9
simplykevine <umutonikevine2004@gmail.com>
2023-11-08 17:59:29 +0000

    python_input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-''' writes a string to a text file (UTF8) and
-returns the number of characters written
-'''
+'''writes a string to a text file (UTF8) and
+returns the number of characters written'''
 
 
-def number_of_lines(filename=""):
-    ''' function: number_of_lines
-    '''
-    if filename == "" or type(filename) != str:
-        return 0
-    counter = 0
-    with open(filename, 'r') as f:
-        for line in f:
-            counter += 1
-    return counter
+def write_file(filename="", text=""):
+    '''function to write text to a file'''
+    with open(filename, 'w+') as f:
+        return f.write(text)
